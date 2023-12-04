00ad77a5a24cdcc105f4fa51f0a1c9a504ff55f7
sok <mohamed.kk878@gmail.com>
2023-12-04 13:23:05 +0200

    js lec

--- js/lec2/lec/main.js
+++ js/lec2/lec/main.js
@@ -15,7 +15,7 @@ let arr2=new Array(30,50,'a',true)
 //     console.log(arr2[i])
 // }
 
-//! dont use associative array 
+//! don't use associative array ==> use objects
 
 let arr3=[]
 arr3['id'] = 1;
@@ -37,9 +37,9 @@ console.log(arr)
 console.log(arr.unshift(90)); //~new length
 console.log(arr)
 
-//? splice take three params  first--> the start to delet sec-->how much to delete the rest is to add
+//? splice take three params  first--> the start to delet sec-->how much to delete ,the rest is to add
 
-console.log(arr.splice(0,2,-40,'hh',500,900,600)) //~return deleted elements or empty if not
+console.log(arr.splice(0,2,-40,'hh',500,330,900,600)) //~return deleted elements or empty if not
 console.log(arr)
 
 //~Delete methods
@@ -75,7 +75,12 @@ console.log(arr)
 
 //~filter methods
 let fil = arr.filter((i)=>i%2===0)
-console.log(fil)
+console.log(fil+" --> odd filter")
+let filt = arr.filter((i)=>typeof i ==="number")
+console.log(filt+" --> nums filter")
+let filts = arr.filter((i)=>typeof i ==="string")
+console.log(filts+" --> string filter")
+
 
 //~reduce take elements from array and do any thing
 
@@ -92,3 +97,77 @@ console.log(sum)
 
 let summred = arr.reduce((s,z)=>s+=z)
 console.log(summred)
+
+//~concat
+let arrx = ['a','c','v']
+let arrz = [1,2,3,4,5]
+
+let narr = arrx.concat(arrz)
+
+console.log(narr)
+
+//~to string
+console.log(narr.toString())
+
+//~join
+
+let jarr = narr.join("-")
+console.log(jarr)
+
+//~indexof-->index of element or -1 and lastindexof-->index and includes -->T + F
+
+//!some, some, every , map , search , flat--self S
+
+
+//~regex
+
+// var nameformate = /^[A-Za-z]+$/
+// do{
+//     var inp = prompt(' your name')
+//     console.log('invaled')
+// }while(! inp.match(nameformate))
+
+var x = new RegExp("^[a-z]{5}")
+var inp = "aliss"
+// console.log(x.test(inp) +" Test") -->tf
+console.log(x.exec(inp) +" Test") //--> array with matched else null
+console.log(inp.match(x)); //--> array with matched else null
+
+//&String
+//?letr
+let str = "xmohamed"
+console.log(str)
+console.log(str[0])
+console.log(str.constructor.name)
+
+//?const cr
+let strr = new String("zmohamed aj")
+console.log(strr)
+console.log(strr[0])
+console.log(strr.constructor.name)
+//?.replace take char or index or regex
+let title = "iti&gmail.com"
+console.log(title.replace('i',"-"))
+console.log(title.replace(/i/gi,"-"))
+console.log(title)
+//?char at
+console.log(title.charAt(3))
+//?index of
+console.log(title.indexOf('i'))
+
+//?last index of
+console.log(title.lastIndexOf("i"))
+
+//?slice
+
+console.log(title.slice(1,7))
+
+//?substring
+
+console.log(title.substring(7,1))
+
+//?split
+let kk="mohamed ahmed -abd elg@ni"
+console.log(kk.split('-'))
+
+
