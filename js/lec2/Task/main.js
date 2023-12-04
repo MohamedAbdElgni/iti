3c453baaaa9f719d5fb125365553e6178d4f8178
sok <mohamed.kk878@gmail.com>
2023-12-05 00:39:42 +0200

    task2jsadde

--- js/lec2/Task/main.js
+++ js/lec2/Task/main.js
@@ -29,15 +29,17 @@ function task2(){
     document.write("<h1>" + "Welcome to my page " + uname + "</h1>");
 }
 
+//^ 1.3
 function task3(){
     let uemail;
     do{
         uemail = prompt("Please Enter Your Email:- ")
-    }while(!uemail || (!uemail.includes("@")|| uemail.indexOf('@')===0 ||uemail.lastIndexOf('@')===uemail.length-1|| uemail.indexOf('@')===uemail.length-1))
+    }while(!uemail || (!uemail.includes("@")|| uemail.indexOf('@')===0 || uemail.lastIndexOf('@')===uemail.length-1 || uemail.indexOf('@')===uemail.length-1))
 
     document.write("<h1>" + "Your email ==> " + uemail + "</h1>");
 }
 
+//^ 1.4
 function task4(){
     let uname
     let ubirth
@@ -58,15 +60,39 @@ function task4(){
         }else{
             alert("Invalid birth year.");}
     }while(true);
-
-    console.log(uname)
-    console.log(ubirth)
+    dateNow = new Date().getFullYear()
+    document.write(`<p><b><u>Name</u></b>: ${uname}</p><p><b><u>Birth year</u></b>: ${ubirth}</p><p><b><u>Age</u></b>: ${dateNow-ubirth}</p>`);
 }
 
+//^ 1.5
+function task5(){
+    
+    let mailVal = new RegExp(/^([A-Za-z]\w*(?:_[A-Za-z0-9.]+)*)@[A-Za-z0-9]+\.[A-Za-z]{2,3}\.eg$/i);
+    let naVal = new RegExp(/^[A-Za-z]{3,}( [A-Za-z]{3,})*$/i);
+    let uemail
+    let uname
+
+    while (!(naVal.test(uname = prompt("Enter your full name:")))){
+        alert("Enter a valid full Name sperated by space");
+    };
+    
+    while (!(mailVal.test(uemail = prompt("Enter your Email:")))){
+        alert(`${uemail} is not a valid Email Ex:- example@mail.net.eg`);
+    };
+
+    document.write(`<p style="text-transform:capitalize;"><b><u>First Name</u></b>: ${uname.split(' ')[0]}</p><p style="text-transform:capitalize;"><b><u>Last Name</u></b>: ${uname.split(' ').length > 1 ? uname.split(' ').slice(1).join(' '):''}</p><p><b><u>Email</u></b>: ${uemail}</p>`);
+}
+
+
+//^1.1
 // task1()
-
+//^1.2
 // task2()
-
+//^1.3
 // task3()
+//^1.4
+// task4()
+//^1.5
+// task5()
 
-task4()
+console.log('B'==='b')
