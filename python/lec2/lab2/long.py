bf9a0c11d7855614fe179740ec24f462bb0799f9
MohamedAbdElgni <mohamed.kk878@gmail.com>
2024-02-14 13:07:14 +0200

    Lab2 python Done

--- python/lec2/lab2/long.py
+++ python/lec2/lab2/long.py
@@ -1,8 +1,23 @@
 def long(str):
-    str = str.split()
-    
-    mylist = []
-    temp = []
-    pass
+    ar1= list(str.lower())
+    temp_str=""
+    i = 0
+    while i < len(ar1)-1:
+        letter = ar1[i]
+        if len(temp_str) ==0:
+            temp_str += letter
+        else:
+            if temp_str[-1] < ar1[i+1]:
+                temp_str +=letter
+            else:
+                temp_str += " "+letter
+        i += 1
+    temp_str= temp_str.split(" ")
+    max = 0
+    for i in temp_str:
+        if len(i) > max:
+            max = len(i)
+            word = i
+    return f"Longest substring is: {word}"
 
-str="abdulrahman"
+print(long("abdulrahman")) 
